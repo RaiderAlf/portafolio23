df82c91d7396d7b593ed3749d33fcb30ec433730
AlfonzoDev <kalfonzoespinett@gmail.com>
2023-09-06 18:20:46 -0400

    responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ function Contact() {
                     <div className='container-fluid p-4 rounded-2 bg-dark'>
                         <h3 className='text-white-50'>Contactame</h3>
                         <form action="">
-                            <div className="mb-3">
+                            <div className="container-fluid mb-3">
                                 <label htmlFor="exampleFormControlInput1" className="form-label text-light">Email</label>
                                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                             </div>
-                            <div className="mb-3">
+                            <div className="container-fluid mb-3">
                                 <label htmlFor="exampleFormControlTextarea1" className="form-label text-light"></label>
                                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
